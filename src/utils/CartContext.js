8b8f63eaf5ba3b044e02fdc25b6d4c538d9c6771
AshishGuggilam <ashishguggilam02@gmail.com>
2023-12-13 00:22:16 +0530

    Cart context added

---
+++ src/utils/CartContext.js
@@ -0,0 +1,23 @@
+import React,{createContext, createContext,useState} from "react";
+const createContext=createContext();
+
+const CartProvider = ({ children }) => {
+    const [cart, setCart] = useState([]);
+  
+    const addToCart = (item) => {
+      setCart([...cart, item]);
+    };
+  
+    const removeFromCart = (itemToRemove) => {
+      const updatedCart = cart.filter((item) => item !== itemToRemove);
+      setCart(updatedCart);
+    };
+  
+    return (
+      <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+        {children}
+      </CartContext.Provider>
+    );
+  };
+  
+  export { CartProvider, CartContext };
